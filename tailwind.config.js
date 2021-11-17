c366e44519f714de1b6e131240ec3cc862d63868
bardenha <bardenha@gmail.com>
2021-11-17 21:05:49 +0000

    :wrench: config: tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,14 @@ module.exports = {
   mode: "jit",
   purge: ["./*.html", "./src/**/*.{css}"],
   plugins: [forms],
+  theme: {
+    extend: {
+      colors: {
+        "light-text": "#1D1D1D",
+        "light-theme": "#FFFFFF",
+        "dark-text": "#F3F3F3",
+        "dark-theme": "#111827",
+      },
+    },
+  },
 };
