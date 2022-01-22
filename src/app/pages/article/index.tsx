6081675d72ad5220ecbc62d4690f65cf79876483
bardenha <bardenha@gmail.com>
2022-01-22 13:19:46 +0000

    :lipstick: style: article comments

--- src/app/pages/article/index.tsx
+++ src/app/pages/article/index.tsx
@@ -6,7 +6,6 @@ import SuspenseLoader from "components/organisms/suspense-loader";
 import ReactMarkdown from "react-markdown";
 
 import { profile } from "components/types";
-
 interface articlePost {
   slug: string;
   title: string;
@@ -19,14 +18,6 @@ interface articlePost {
   favoritesCount: number;
   author: profile;
 }
-
-// const article2 = {
-//   title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//   excerpt:
-//     "Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit",
-//   content:
-//     "Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elitipsum dolor sit amet consectetur adipisicing elit",
-// };
 
 export default function Article() {
   const { articleSlug } = useParams();
@@ -82,10 +73,7 @@ export default function Article() {
             <h1 className={styles.headerTitle}>{article.title}</h1>
             <div className={styles.authorContainer}>
               <div className={styles.imageContainer}>
-                <img
-                  src="https://avatars.dicebear.com/api/avataaars/newuser.svg?topChance=100&eyes[]=default&eyes[]=wink&eyes[]=happy&eyebrow[]=defaultNatural&eyebrow[]=default&mouth[]=smile&mouth[]=default"
-                  alt="User profile image"
-                />
+                <img src={article.author.image} alt="User profile image" />
               </div>
               <div className={styles.authorInfo}>
                 <h5>
@@ -106,10 +94,62 @@ export default function Article() {
             </ReactMarkdown>
           </section>
           <section className={styles.commentSection}>
-            <p>{articleSlug}</p>
+            <Comments article={article} />
           </section>
         </>
       )}
     </div>
   );
 }
+
+const Comments: React.FC<{ article: articlePost }> = ({ article }) => {
+  return (
+    <>
+      <div className={styles.writeComment}>
+        <h2>Comments ({10})</h2>
+        <button className={styles.writeCommentButton}>
+          <PlusIcon />
+          Write a comment
+        </button>
+      </div>
+      <div className={styles.comment}>
+        <div className={styles.commentAuthorContainer}>
+          <div className={styles.commentImageContainer}>
+            <img src={article.author.image} alt="User profile image" />
+          </div>
+          <div className={styles.authorInfo}>
+            <h5>
+              <Link to={`/user/${article.author.username}`}>
+                {article.author.username}
+              </Link>
+            </h5>
+            <small>Aug 31</small>
+          </div>
+        </div>
+        <p className={styles.commentDescription}>
+          Awesome Kieran Roberts! When I first started, I felt the same way
+          about doing things reactively rather than proactively. I'm glad to
+          hear that you're now taking on new challenges and growing a lot! Keep
+          it up, you're doing just great 🎉
+        </p>
+      </div>
+    </>
+  );
+};
+
+const PlusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-1 h-1"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
